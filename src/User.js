3294351c2bb10c31c08c1102d1bb85649e5e28b4
Carlyle Douglas <cdouglascorp@gmail.com>
2020-07-12 19:52:11 -0600

    Add method to allow user to search saved recipes by their category

--- src/User.js
+++ src/User.js
@@ -1,7 +1,7 @@
 //Note: the below line breaks scripts.js
 //If you comment it out, scripts.js works, but test no longer works
 //Look into this more (other groups have same problem)
-// const Pantry = require('../src/Pantry');
+const Pantry = require('../src/Pantry');
 
 class User {
   constructor(name, id, pantry) {
@@ -34,10 +34,14 @@ class User {
   getSavedRecipes() {
     return this.favoriteRecipes.concat(this.recipesToCook);
   }
+
+  filterRecipeByTag(tag) {
+    let savedRecipes = this.getSavedRecipes()
+    return savedRecipes.filter(recipe => {
+      return recipe.tags.includes(tag)
+    })
+  }
 }
-
-
-
 if (typeof module !== 'undefined') {
   module.exports = User;
 }
